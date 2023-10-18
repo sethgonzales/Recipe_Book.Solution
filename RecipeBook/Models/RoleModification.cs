c3cf822c15a4d7fcdb5e1ce3052ef4cf3954b0f9
sethgonzales <sethgonzales157@gmail.com>
2023-10-18 16:15:18 -0700

    finished the CRUD

---
+++ RecipeBook/Models/RoleModification.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBook.Models
+{
+    public class RoleModification
+    {
+        [Required]
+        public string RoleName { get; set; }
+
+        public string RoleId { get; set; }
+#nullable enable
+        public string[]? AddIds { get; set; }
+
+        public string[]? DeleteIds { get; set; }
+#nullable disable
+    }
+}
